7dd3c99a610853e7676313aba229a43406118719
Simon Jefford <si@ibrokethat.com>
2012-08-30 22:19:01 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+/**
+  @module  async sequence pipeline based upon promises
+*/
+
+var async   = require("ibt-async"),
+    func    = require("ibt-func"),
+    Promise = async.Promise,
+    when    = async.when,
+    bind    = func.bind,
+    partial = func.partial;
+
+
+/**
+  @description  Binds incoming and outgoing parameters from a data
+                structure composed of functions onto a sequence of promises
+                Enables chaining functions that return a value
+                with functions that return a promise as all function responses
+                are coerced into being promises.
+                Kind of monadish without really being one
+  @param        {function} unit
+  @param        {promise} output
+  @param        {object} next
+  @param        {any} input
+*/
+function step(unit, output, next, input) {
+
+  function process(response, method, value) {
+
+    if (next) {
+      step(unit, output, next[response][1], next[response][0](value));
+    }
+    else {
+      output[method](value);
+    }
+
+  }
+
+  unit(input).then(
+    partial(process, "CMD_OK", "resolve"),
+    partial(process, "CMD_ERROR", "reject")
+  );
+
+}
+
+
+
+/**
+  @description  Composes a function from unit & step (bind) functions
+                and a data structure representing a sequence of functions.
+  @param        {function} step
+  @param        {function} unit
+  @param        {object} sequence
+  @return       {promise}
+*/
+function sequence(step, unit, sequence) {
+
+  return function (value) {
+
+    var output = Promise.spawn();
+
+    step(unit, output, sequence[1], sequence[0](value));
+
+    return output;
+
+  }
+
+}
+
+
+exports.seq      = partial(sequence, step, when);
+exports.step     = step;
+exports.sequence = sequence;
---
+++ test/index.js
@@ -0,0 +1,348 @@
+var assert    = require("assert"),
+    sinon     = require("sinon"),
+    sequences = require("../index"),
+    async     = require("ibt-async"),
+    seq       = sequences.seq,
+    step      = sequences.step,
+    sequence  = sequences.sequence,
+    Promise   = async.Promise,
+    fakes;
+
+
+function makeResolver() {
+  return sinon.spy(function (v) {
+    var p = Promise.spawn();
+    setTimeout(function(){
+      p.resolve(v);
+    }, 10);
+    return p;
+  });
+}
+
+function makeRejector() {
+  return sinon.spy(function (v) {
+    var p = Promise.spawn();
+    setTimeout(function(){
+      p.reject(v);
+    }, 10);
+    return p;
+  });
+}
+
+
+describe("test seq module: ", function() {
+
+
+  beforeEach(function() {
+
+    fakes = sinon.sandbox.create();
+
+  });
+
+  afterEach(function() {
+
+    fakes.restore();
+
+  });
+
+
+  describe("function seq", function() {
+
+
+    it("should run an async sequence where everything resolves", function(done) {
+
+      var s, logUserIn;
+
+      s = [makeResolver(), {
+        "CMD_OK": [makeResolver(), {
+          "CMD_OK": [makeResolver(), {
+              "CMD_OK": [makeResolver()],
+              "CMD_ERROR": [makeResolver()]
+              },
+          ],
+          "CMD_ERROR": [makeResolver()]
+        }],
+        "CMD_ERROR": [makeResolver()]
+      }];
+
+      logUserIn = seq(s);
+
+      logUserIn("simon").then(function(value) {
+
+        assert.equal("simon", value);
+        assert.equal(1, s[0].callCount);
+        assert.equal(1, s[1].CMD_OK[0].callCount);
+        assert.equal(1, s[1].CMD_OK[1].CMD_OK[0].callCount);
+        assert.equal(1, s[1].CMD_OK[1].CMD_OK[1].CMD_OK[0].callCount);
+
+        assert.equal(0, s[1].CMD_ERROR[0].callCount);
+        assert.equal(0, s[1].CMD_OK[1].CMD_ERROR[0].callCount);
+        assert.equal(0, s[1].CMD_OK[1].CMD_OK[1].CMD_ERROR[0].callCount);
+
+        done();
+      });
+
+    });
+
+
+    it("should run an async sequence where the first function rejects", function(done) {
+
+      var s, logUserIn;
+
+      s = [makeRejector(), {
+        "CMD_OK": [makeResolver(), {
+          "CMD_OK": [makeResolver(), {
+              "CMD_OK": [makeResolver()],
+              "CMD_ERROR": [makeResolver()]
+              },
+          ],
+          "CMD_ERROR": [makeResolver()]
+        }],
+        "CMD_ERROR": [makeResolver()]
+      }];
+
+      logUserIn = seq(s);
+
+      logUserIn("simon").then(
+        function(value) {
+
+          assert.equal("simon", value);
+          assert.equal(1, s[0].callCount);
+          assert.equal(0, s[1].CMD_OK[0].callCount);
+          assert.equal(0, s[1].CMD_OK[1].CMD_OK[0].callCount);
+          assert.equal(0, s[1].CMD_OK[1].CMD_OK[1].CMD_OK[0].callCount);
+
+          assert.equal(1, s[1].CMD_ERROR[0].callCount);
+          assert.equal(0, s[1].CMD_OK[1].CMD_ERROR[0].callCount);
+          assert.equal(0, s[1].CMD_OK[1].CMD_OK[1].CMD_ERROR[0].callCount);
+
+          done();
+
+        },
+        function(value) {
+
+        }
+      );
+
+    });
+
+
+    it("should run an async sequence where the second function rejects", function(done) {
+
+      var s, logUserIn;
+
+      s = [makeResolver(), {
+        "CMD_OK": [makeRejector(), {
+          "CMD_OK": [makeResolver(), {
+            "CMD_OK": [makeResolver()],
+            "CMD_ERROR": [makeResolver()]
+          }],
+          "CMD_ERROR": [makeResolver()]
+        }],
+        "CMD_ERROR": [makeResolver()]
+      }];
+
+
+      logUserIn = seq(s);
+
+      logUserIn("simon").then(
+        function(value) {
+
+          assert.equal("simon", value);
+          assert.equal(1, s[0].callCount);
+          assert.equal(1, s[1].CMD_OK[0].callCount);
+          assert.equal(0, s[1].CMD_OK[1].CMD_OK[0].callCount);
+          assert.equal(0, s[1].CMD_OK[1].CMD_OK[1].CMD_OK[0].callCount);
+
+          assert.equal(0, s[1].CMD_ERROR[0].callCount);
+          assert.equal(1, s[1].CMD_OK[1].CMD_ERROR[0].callCount);
+          assert.equal(0, s[1].CMD_OK[1].CMD_OK[1].CMD_ERROR[0].callCount);
+
+          done();
+        },
+        function() {}
+      );
+
+    });
+
+    it("should run an async sequence where the third function rejects", function(done) {
+
+      var s, logUserIn;
+
+      s = [makeResolver(), {
+        "CMD_OK": [makeResolver(), {
+          "CMD_OK": [makeRejector(), {
+            "CMD_OK": [makeResolver()],
+            "CMD_ERROR": [makeResolver()]
+          }],
+          "CMD_ERROR": [makeResolver()]
+        }],
+        "CMD_ERROR": [makeResolver()]
+      }];
+
+
+      logUserIn = seq(s);
+
+      logUserIn("simon").then(
+        function(value) {
+
+          assert.equal("simon", value);
+          assert.equal(1, s[0].callCount);
+          assert.equal(1, s[1].CMD_OK[0].callCount);
+          assert.equal(1, s[1].CMD_OK[1].CMD_OK[0].callCount);
+          assert.equal(0, s[1].CMD_OK[1].CMD_OK[1].CMD_OK[0].callCount);
+
+          assert.equal(0, s[1].CMD_ERROR[0].callCount);
+          assert.equal(0, s[1].CMD_OK[1].CMD_ERROR[0].callCount);
+          assert.equal(1, s[1].CMD_OK[1].CMD_OK[1].CMD_ERROR[0].callCount);
+
+          done();
+        },
+        function() {}
+      );
+
+    });
+
+    it("should run an async sequence where the last function rejects", function(done) {
+
+      var s, logUserIn;
+
+      s = [makeResolver(), {
+        "CMD_OK": [makeResolver(), {
+          "CMD_OK": [makeResolver(), {
+            "CMD_OK": [makeRejector()],
+            "CMD_ERROR": [makeResolver()]
+          }],
+          "CMD_ERROR": [makeResolver()]
+        }],
+        "CMD_ERROR": [makeResolver()]
+      }];
+
+
+      logUserIn = seq(s);
+
+      logUserIn("simon").then(
+        function() {},
+        function(value) {
+
+          assert.equal("simon", value);
+          assert.equal(1, s[0].callCount);
+          assert.equal(1, s[1].CMD_OK[0].callCount);
+          assert.equal(1, s[1].CMD_OK[1].CMD_OK[0].callCount);
+          assert.equal(1, s[1].CMD_OK[1].CMD_OK[1].CMD_OK[0].callCount);
+
+          assert.equal(0, s[1].CMD_ERROR[0].callCount);
+          assert.equal(0, s[1].CMD_OK[1].CMD_ERROR[0].callCount);
+          assert.equal(0, s[1].CMD_OK[1].CMD_OK[1].CMD_ERROR[0].callCount);
+
+          done();
+        }
+      );
+
+    });
+
+
+
+    it("should run a sequence composed of both synchronous and async functions", function(done) {
+
+      var s, logUserIn;
+
+      s = [makeResolver(), {
+        "CMD_OK": [sinon.stub().returns("simon"), {
+          "CMD_OK": [makeResolver(), {
+              "CMD_OK": [sinon.stub().returns("simon")],
+              "CMD_ERROR": [makeResolver()]
+              },
+          ],
+          "CMD_ERROR": [makeResolver()]
+        }],
+        "CMD_ERROR": [makeResolver()]
+      }];
+
+      logUserIn = seq(s);
+
+      logUserIn("simon").then(
+        function(value) {
+
+          assert.equal("simon", value);
+          assert.equal(1, s[0].callCount);
+          assert.equal(1, s[1].CMD_OK[0].callCount);
+          assert.equal(1, s[1].CMD_OK[1].CMD_OK[0].callCount);
+          assert.equal(1, s[1].CMD_OK[1].CMD_OK[1].CMD_OK[0].callCount);
+
+          assert.equal(0, s[1].CMD_ERROR[0].callCount);
+          assert.equal(0, s[1].CMD_OK[1].CMD_ERROR[0].callCount);
+          assert.equal(0, s[1].CMD_OK[1].CMD_OK[1].CMD_ERROR[0].callCount);
+
+          done();
+        },
+        function(value) {}
+      );
+
+    });
+
+
+    it("should reject it's output promise if the last function in the sequence rejects", function(done) {
+
+      var s, logUserIn;
+
+      s = [makeResolver(), {
+        "CMD_OK": [makeResolver(), {
+          "CMD_OK": [makeRejector(), {
+            "CMD_OK": [makeResolver()],
+            "CMD_ERROR": [makeRejector()]
+          }],
+          "CMD_ERROR": [makeResolver()]
+        }],
+        "CMD_ERROR": [makeResolver()]
+      }];
+
+
+      logUserIn = seq(s);
+
+      logUserIn("simon").then(
+        function() {},
+        function(value) {
+
+          assert.equal("simon", value);
+
+          done();
+        }
+      );
+
+    });
+
+
+    it("should reject it's output promise if the last function in the sequence rejects", function(done) {
+
+      var s, logUserIn;
+
+      s = [makeResolver(), {
+        "CMD_OK": [makeResolver(), {
+          "CMD_OK": [makeRejector(), {
+            "CMD_OK": [makeResolver()],
+            "CMD_ERROR": [makeRejector()]
+          }],
+          "CMD_ERROR": [makeResolver()]
+        }],
+        "CMD_ERROR": [makeResolver()]
+      }];
+
+
+      logUserIn = seq(s);
+
+      logUserIn("simon").then(
+        function() {},
+        function(value) {
+
+          assert.equal("simon", value);
+
+          done();
+        }
+      );
+
+    });
+
+  });
+
+});
